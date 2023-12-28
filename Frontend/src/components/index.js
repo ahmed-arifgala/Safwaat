f07ffc42a64750056895c46927aaedd71e2d4367
MZubair1512 <mzubair15121716@gmail.com>
2023-12-28 07:11:31 +0500

    Delete Frontend/src directory

--- Frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import LoginDialog from "./loginDialog";
-import InputWL from "./input";
-import SignDialog from "./signupDialog";
-import Button from "./button";
-import Checkbox from "./checkbox";
-export {LoginDialog,InputWL,SignDialog,Button,Checkbox}
