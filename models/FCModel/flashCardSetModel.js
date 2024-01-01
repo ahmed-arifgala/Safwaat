699730df8f70e74b64cb72753bb3f55a18efe976
arsalan1004 <syedarsalan3505@gmail.com>
2024-01-01 10:44:48 +0500

    feat: add FlashCraft controllers and models

---
+++ models/FCModel/flashCardSetModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const FlashCardSetSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    flashCards: Array,
+  },
+  {
+    collection: "FlashCardSet",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("FlashCardSet", FlashCardSetSchema);
